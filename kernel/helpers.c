20f77d459bf1ccf8c322930b86d5dd2d6182536d
Pavel Valov <valov.pm@gmail.com>
2013-01-22 18:03:32 -0500

    wbprint fix

--- kernel/helpers.c
+++ kernel/helpers.c
@@ -4,10 +4,10 @@
 
 
 void assert( int statement, char * err_msg ) {
-	if( !statement ) wbprintf( COM2, "Assertion failed: %s\n\r", err_msg );
+	if( !statement ) bwprintf( COM2, "Assertion failed: %s\n\r", err_msg );
 }
 
 void panic( char * panic_reason ) {
-	wbprintf( COM2, "Kernel panic: %s\n\r", panic_reason );
+	bwprintf( COM2, "Kernel panic: %s\n\r", panic_reason );
 	for( ; ; );
 }

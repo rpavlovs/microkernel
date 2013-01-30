3fe34dce94fdc24de92c2891d318e6e7da2968ed
alex <alex@ubuntu>
2013-01-30 08:09:40 -0500

    Initial RPS server implementation

--- tasks/RPS-game.c
+++ tasks/RPS-game.c
@@ -1,11 +1,58 @@
 
+// Operation numbers
+#define RPS_OP_SIGNUP                   0
+#define RPS_OP_PLAY                     1
+#define RPS_OP_QUIT                     2
 
+// Return values
+#define 
 
+// Buffers
+#define RPS_MAX_RECEIVE_BUFFER_SIZE     2
 
+/*
+ *      RPS Messages:
+ * 
+ * 
+ *      [OP][Parameter]
+ *      OP -> 1 char -> Can be Signup, play or quit. 
+ *      Parameter -> 1 char -> Only used for play; the client sends here the 
+ */
 
-
-void client() {
-
+void server_entry_point()
+{
+    // Variables Initialization.
+    int sender_tid;
+    int msg_size; 
+    
+    debug( "STARTING RPS Server. " ); 
+    FOREVER{
+        
+        msg_size = Receive( &sender_tid, msg, RPS_MAX_BUFFER_SIZE );
+        bwprintf( COM2, "DEBUG: RPS Server: Received new request. TID: %d OP: [%d] Parameter: [%d]\n",
+			sender_tid, msg[0], msg[1] );
+        
+        int operation_id = msg[0]; 
+        int operation_param = msg[1]; 
+        
+        switch ( operation_id )
+        {
+            case RPS_OP_SIGNUP:
+                
+                break;
+            case RPS_OP_PLAY:
+                
+                break;
+            case RPS_OP_QUIT:
+                
+                break;
+            case default:
+                debug( "RPS ERROR: Invalid Operation ID. " ); 
+                break;
+        }
+    }
+    
+    debug( "EXITING RPS Server. " ); 
 }
 
 void server() {

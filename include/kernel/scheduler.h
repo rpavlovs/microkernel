20d069b7ea88821429972926d75e0a9148af678a
Pavel Valov <valov.pm@gmail.com>
2013-02-03 19:36:18 -0500

    Adding files for new scheduler

--- include/kernel/scheduler.h
+++ include/kernel/scheduler.h
@@ -3,36 +3,23 @@
 
 #include "kernelspace.h"
 
-
 // Description:
-// Init schedule structure in the kernel globals.
-// Specify the first task to be executed and it's priority 
+// Represents a node in main priority queue
+typedef struct Node
+{
+    Task_descriptor *td;
+    struct Node *next;
+    struct Node *previous;
+} Node;
 
-void init_schedule( int, void (*) ( ), Kern_Globals * );
-
-// Description:
-// Find an unused task descriptor
-// Set it up for a new task
-// Add it to the specific scheduler queue 
-//
-// Return:
-// task id of the new process
-// -1 if wrong task priority was given
-// -2 if no task descriptors left
-
-int add_task( int, void (*) ( ), Kern_Globals * );
-
-// Description:
-// Run the next scheduled task until interrupt recieved
-// 
-// Return:
-// Interrupt id 
-
-int getNextRequest( Kern_Globals * );
-
-// TODO: Description
-//       
-
-void handle_request( int, Kern_Globals * );
+// Description
+// Represents main priority queue
+typedef struct
+{
+    Node nodes[102];
+    Node *deadend;
+    Node *sentinel;
+    Node *last_in_priority[16];
+} Priority_queue;
 
 #endif

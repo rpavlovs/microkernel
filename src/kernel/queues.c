14cd6f6cee20fdbf1f0c6f68e74ac61623e66e22
Pavel Valov <valov.pm@gmail.com>
2013-02-04 20:44:06 -0500

    Refactoring enqueue_tqueue. Completed

--- src/kernel/queues.c
+++ src/kernel/queues.c
@@ -2,30 +2,27 @@
 
 void enqueue_tqueue(Task_descriptor *td, Task_queue *q){
 	// ASSERT: Verifying the size of the queue
-	assert( q->size < SCHED_QUEUE_LENGTH, "Scheduler queue must not be full" );
+	assert( q->size < SCHED_QUEUE_LENGTH, "Task queue should not overflow :)" );
 
-	// If the queue is empty or the newest pointer is at the end of the td_ptrs buffer
-	// put the next td_ptr at the beginning on the buffer  
-	if (q->size == 0 || ++(q->newest) >= SCHED_QUEUE_LENGTH) q->newest = 0;
-	
-	// If the queue was empty then newest and oldest elements are the same 
-	// and are at the beginning of the buffer
-	if (q->size == 0) q->oldest = 0;
+	//Checking for empty queue
+	if (q->size == 0)
+	{
+		q->newest = 0;
+		q->oldest = 0;
+	}
+	//Checking for queue boundaries
+	else if (++(q->newest) >= SCHED_QUEUE_LENGTH)
+	{
+		q->newest = 0;
+	}
 
 	// Updating the queue
 	q->size++;
 	q->td_ptrs[q->newest] = td;
-
-	/*assert(q->size != MAX_NUM_TASKS, "Task queue should not overflow :)");
-
-	//Modifying the queue
-	q->size++;
-	if(++(q->newest) == MAX_NUM_TASKS) q->newest = 0;
-	q->td_ptrs[q->newest] = td;*/
 }
 
 Task_descriptor *dequeue_tqueue(Task_queue *q){
-	assert(q->size != 0, "Task queue should have items to dequeue");
+	assert( q->size > 0, "Task queue should have items to dequeue" );
 
 	//Modifying the queue
 	q->size--;

19bb38491b755b7da56917c50f271a1c93069ed3
Roman Pavlovskyi <wilohvost@gmail.com>
2013-02-23 22:13:10 -0500

    partial cli done

--- src/tasks/cli.c
+++ src/tasks/cli.c
@@ -1,6 +1,22 @@
 #include "userspace.h"
 
-void task_cli() {
+void update_cli( const CLI_history history ) {
 
+}
+
+char *getCommand() {
+	char next_char;
+	while( next_char = Getc(COM2) != "\n" ) {
+
+	}
+}
+
+void task_cli() {
+	CLI_history history;
+	history.length = 0;
+
+	FOREVER {
+		getCommand();
+	}
 
 }
